1aab7dd1845690380e6e86f54febca7ae8fea6d3
syahidanfitrah <syahidanfitrah9@gmail.com>
2022-06-09 21:09:05 +0700

    Tubes

--- tubes/detail.php
+++ tubes/detail.php
@@ -26,6 +26,9 @@ if (isset($_GET["cari"])) {
     html {
       scroll-behavior: smooth;
     }
+    body {
+      background-color: black;
+    }
   </style>
 </head>
 
@@ -35,15 +38,17 @@ if (isset($_GET["cari"])) {
       <a class="navbar-brand" href="index.php">
         <img src="img/Logo.png" width="70px" alt="">
       </a>
-      <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
+      <div class="tombol">
+      <button data-toggle="collapse" class="navbar-toggler btn btn-warning bg-warning" data-target="#navcol-1">
         <span class="sr-only">Toggle navigation</span>
-        <span class="navbar-toggler-icon">
+        <span class="navbar-toggler-icon"></span>
+    </div>
 
         </span>
       </button>
       <div class="collapse navbar-collapse" id="navcol-1">
         <ul class="nav navbar-nav">
-          <li class="nav-item" role="presentation" class="fw-bold"><a class="nav-link" href="index.php" >Home</a></li>
+          <li class="nav-item" role="presentation" class="fw-bold"><a class="nav-link text-warning" href="index.php" >Home</a></li>
         </ul>
       </div>
     </div>
@@ -54,7 +59,7 @@ if (isset($_GET["cari"])) {
           <img src="img/<?= $row['gambar']; ?>" width="500px" alt="" class=" mx-auto d-block">
         </div>
       </div>
-      <div class="row">
+      <div class="row text-warning">
         <div class="col">
           <h1 class="mb-5"><?= $row['judul']; ?></h1>
           <h2><?= $row['tahun_rilis']; ?></h2>
@@ -62,9 +67,19 @@ if (isset($_GET["cari"])) {
           <h2><?= $row['genre']; ?></h2>
           <h4 class="text-center" >Storyline</h4>
           <h4 class="text-center"><?= $row['sinopsis']; ?></h4>
-          <a href="index.php" class="btn btn-primary mt-3 shadow">Back</a>
+          <a href="index.php" class="btn btn-warning mt-3 shadow">Back</a>
         </div>
       </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bs-animation.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+  <script src="js/stylish-portfolio.js"></script>
+  <script src="js/script.js"></script>
 </body>
 </html>
--- tubes/index.php
+++ tubes/index.php
@@ -119,7 +119,15 @@ if ( isset($_POST['cari']) ) {
     </footer>
   </div>
 
-  <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bs-animation.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+  <script src="js/stylish-portfolio.js"></script>
   <script src="js/script.js"></script>
 </body>
 
